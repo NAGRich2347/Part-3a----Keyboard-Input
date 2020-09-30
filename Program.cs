c66e7d606f4e72e16817b9881a2693b165844809
Noah Richardson <RichNoah983@GoTVDSB.ca>
2020-09-30 12:43:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Part_3a____Keyboard_Input
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int age;
+            string response;
+            Console.WriteLine("Say Hello!");
+            response = Console.ReadLine();
+            Console.WriteLine("You said: " + response);
+            Console.WriteLine("Please enter your age");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Your age is {age}.");
+            Console.ReadLine();
+        }
+    }
+}
